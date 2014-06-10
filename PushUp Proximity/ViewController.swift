0aa7eece97bc06d619f50984bc11987962b4bfd9
Tyler Calderone <tyler@tylercalderone.com>
2014-06-10 09:15:43 -0700

    Initial Commit

---
+++ PushUp Proximity/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PushUp Proximity
+//
+//  Created by Tyler Calderone on 6/10/14.
+//  Copyright (c) 2014 Tyler Calderone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
